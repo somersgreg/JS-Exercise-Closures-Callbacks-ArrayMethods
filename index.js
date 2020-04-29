d2e699b1a5feb2bd5e8197ceece0d30aa50d2e5e
Greg Somers <somersgreg@yahoo.com>
2020-04-28 22:12:01 -0400

    lowercase done.

--- index.js
+++ index.js
@@ -157,10 +157,13 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * 
  * [2] Invoking `lowerCaseStrings` with `['a', 'b', 'c' ]` will return `[ 'a', 'b', 'c' ]`.
 */
-function lowerCaseStrings(/* code here */) {
-  /* code here */
+function lowerCaseStrings(strings) {
+  const newMapArray = [];
+  strings.forEach(function(string) {
+    newMapArray.push(string.toLowerCase());
+  });
+  return newMapArray;
 }
-
 /**
  * ### Challenge `isItAnApple`
  * 
